944b6e3c98ac95dc44ceb9d7442fc46293135472
James <naughtsee@gmail.com>
2017-06-30 23:56:33 -0400

    added comments

--- getTrailers.py
+++ getTrailers.py
@@ -3,11 +3,14 @@ import os
 import glob
 import re
 
+
 # Global Vars
-append = "-trailer"
-movie_dir = "X:/Videos/Movies/"
-movie_name_list = os.listdir(movie_dir)
-movie_dir_list = []
+append = "-trailer" # Plex requires this
+movie_dir = "X:/Videos/Movies/" # My movie directory
+movie_name_list = os.listdir(movie_dir) # My directories are named {Title} {(Year)} -> this is required
+
+# Empty lists for later
+movie_dir_list = [] 
 missing_trailer_list = []
 downloaded_trailer_list = []
 
@@ -22,28 +25,39 @@ for movie in movie_name_list:
 
 # Download trailers
 for x in range(len(movie_name_list)): 
+    # Check if trailer exists
+    # If so, skip
     if glob.glob("{}*-trailer*".format(movie_dir_list[x])):
         print("Pre-existing trailer for {}. Skipping...".format(movie_name_list[x]))
         continue
-
+    
     vid_nfo = yt.get_video_info("{} trailer".format(movie_name_list[x]), append)
     watch_url = yt.get_watch_url(vid_nfo["id"])
 
+    # Tries to find a matching video
     try:
+        # re.sub removes characters not allowed in filenames on Windows
         yt.download_video(watch_url, re.sub('[<>:|?"]', "", vid_nfo["title"]), directory = movie_dir_list[x])
+        
+        # Save which trailers were downloaded for later
         downloaded_trailer_list.append(movie_name_list[x])
+        
         print("Downloaded trailer for {}. Success!".format(movie_name_list[x]))
-
+    
+    # No videos found / malfunction
     except:
+        # Remember which trailers could not be downloaded
         missing_trailer_list.append(movie_name_list[x])
         print("Fatal error downloading trailer for {}. Skipping...".format(vid_nfo["title"]))
         continue
 
+# If no trailers were downloaded / updated
 if len(missing_trailer_list) == 0 and len(downloaded_trailer_list) == 0:
     print("-----------------------------------------------------------")
     print("Done. All trailers up to date.")
     print("-----------------------------------------------------------")
 
+# If some trailers failed / were updated
 else:
     print("-----------------------------------------------------------")
     print("Downloaded trailers for:")
