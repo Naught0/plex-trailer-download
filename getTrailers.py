ac8836f6770ebad7ea763db10fd3ff2217a2ee48
James <naughtsee@gmail.com>
2017-06-30 23:48:39 -0400

    Initial commit

---
+++ getTrailers.py
@@ -0,0 +1,63 @@
+import yt
+import os
+import glob
+import re
+
+# Global Vars
+append = "-trailer"
+movie_dir = "X:/Videos/Movies/"
+movie_name_list = os.listdir(movie_dir)
+movie_dir_list = []
+missing_trailer_list = []
+downloaded_trailer_list = []
+
+# Filter dotfiles
+for movie in movie_name_list:
+    if movie.startswith('.'):
+        movie_name_list.remove(movie)
+
+# Get movie paths
+for movie in movie_name_list:
+    movie_dir_list.append(movie_dir + movie + "/")
+
+# Download trailers
+for x in range(len(movie_name_list)): 
+    if glob.glob("{}*-trailer*".format(movie_dir_list[x])):
+        print("Pre-existing trailer for {}. Skipping...".format(movie_name_list[x]))
+        continue
+
+    vid_nfo = yt.get_video_info("{} trailer".format(movie_name_list[x]), append)
+    watch_url = yt.get_watch_url(vid_nfo["id"])
+
+    try:
+        yt.download_video(watch_url, re.sub('[<>:|?"]', "", vid_nfo["title"]), directory = movie_dir_list[x])
+        downloaded_trailer_list.append(movie_name_list[x])
+        print("Downloaded trailer for {}. Success!".format(movie_name_list[x]))
+
+    except:
+        missing_trailer_list.append(movie_name_list[x])
+        print("Fatal error downloading trailer for {}. Skipping...".format(vid_nfo["title"]))
+        continue
+
+# Cleanup
+raw_glob_list = []
+for x in range(len(movie_name_list)):
+    raw_glob_list = glob.glob("{}*.temp".format(movie_dir_list[x]))
+print(raw_glob_list)
+
+
+if len(missing_trailer_list) == 0 and len(downloaded_trailer_list) == 0:
+    print("-----------------------------------------------------------")
+    print("Done. All trailers up to date.")
+    print("-----------------------------------------------------------")
+
+else:
+    print("-----------------------------------------------------------")
+    print("Downloaded trailers for:")
+    for x in range(len(downloaded_trailer_list)):
+        print(downloaded_trailer_list[x])
+    print("-----------------------------------------------------------")
+    print("Unable to find trailers for:")
+    print("-----------------------------------------------------------")
+    for x in range(len(missing_trailer_list)):
+        print(missing_trailer_list[x])
---
+++ yt.py
@@ -0,0 +1,42 @@
+import requests
+import os
+import pafy
+import re
+
+yt_api_key="AIzaSyAucnOGE-CIhdCXYt6pzA2ERG7OhMlMgqk"
+
+api_uri = "https://www.googleapis.com/youtube/v3/search?part=snippet&q={}&type=video&key={}"
+
+def get_video_info(query : str, title_append = "") -> dict:
+    """ 
+    Retrives video ID from the youtube API 
+
+    query: Regular ol' search string
+
+    title_append: for appending things to the title of the video
+        e.g. If you're using Plex Media Server and trailers must
+        be labeled with '-trailer'
+    """
+
+    query = query.replace(" ", "+")
+    result = requests.get(api_uri.format(query, yt_api_key)).json()
+    
+    video_id = result["items"][0]["id"]["videoId"]
+    raw_title = result["items"][0]["snippet"]["title"]
+
+    return {"id" : video_id, "title" : raw_title + title_append}
+
+
+def get_watch_url(video_id : str) -> str:
+    """ Creates watchable / downloadable URL from video's ID """
+
+    return "https://www.youtube.com/watch?v={}".format(video_id)
+
+def download_video(video_url, video_title, directory = os.getcwd()):
+    """ Uses pafy to download the video """ 
+
+    video = pafy.new(video_url)
+    hq_video = video.getbest(preftype = "mp4")
+
+    # This bit is ugly but it works so ¯\_(ツ)_/¯ 
+    hq_video.download(filepath = directory + video_title + "." + hq_video.extension, quiet=False)
