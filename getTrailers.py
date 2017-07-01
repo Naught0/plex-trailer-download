983047457f8f3eb445bf134ec88f1268dde3e26a
James <naughtsee@gmail.com>
2017-07-01 14:14:23 -0400

    autopep and add gitignore

--- getTrailers.py
+++ getTrailers.py
@@ -5,12 +5,13 @@ import re
 
 
 # Global Vars
-append = "-trailer" # Plex requires this
-movie_dir = "X:/Videos/Movies/" # My movie directory
-movie_name_list = os.listdir(movie_dir) # All movies must be in separate directories
+append = "-trailer"  # Plex requires this
+movie_dir = "X:/Videos/Movies/"  # My movie directory
+# All movies must be in separate directories
+movie_name_list = os.listdir(movie_dir)
 
 # Empty lists for later
-movie_dir_list = [] 
+movie_dir_list = []
 missing_trailer_list = []
 downloaded_trailer_list = []
 
@@ -24,31 +25,35 @@ for movie in movie_name_list:
     movie_dir_list.append(movie_dir + movie + "/")
 
 # Download trailers
-for x in range(len(movie_name_list)): 
+for x in range(len(movie_name_list)):
     # Check if trailer exists
     # If so, skip
     if glob.glob("{}*-trailer*".format(movie_dir_list[x])):
-        print("Pre-existing trailer for {}. Skipping...".format(movie_name_list[x]))
+        print(
+            "Pre-existing trailer for {}. Skipping...".format(movie_name_list[x]))
         continue
-    
-    vid_nfo = yt.get_video_info("{} trailer".format(movie_name_list[x]), append)
+
+    vid_nfo = yt.get_video_info(
+        "{} trailer".format(movie_name_list[x]), append)
     watch_url = yt.get_watch_url(vid_nfo["id"])
 
     # Tries to find a matching video
     try:
         # re.sub removes characters not allowed in filenames on Windows
-        yt.download_video(watch_url, re.sub('[<>:|?"]', "", vid_nfo["title"]), directory = movie_dir_list[x])
-        
+        yt.download_video(watch_url, re.sub(
+            '[<>:|?"]', "", vid_nfo["title"]), directory=movie_dir_list[x])
+
         # Save which trailers were downloaded for later
         downloaded_trailer_list.append(movie_name_list[x])
-        
+
         print("Downloaded trailer for {}. Success!".format(movie_name_list[x]))
-    
+
     # No videos found / malfunction
     except:
         # Remember which trailers could not be downloaded
         missing_trailer_list.append(movie_name_list[x])
-        print("Fatal error downloading trailer for {}. Skipping...".format(vid_nfo["title"]))
+        print("Fatal error downloading trailer for {}. Skipping...".format(
+            vid_nfo["title"]))
         continue
 
 # If no trailers were downloaded / updated
--- yt.py
+++ yt.py
@@ -3,11 +3,13 @@ import os
 import pafy
 import re
 
-yt_api_key="<youtube-api-key>"
+with open("apikey") as f:
+    yt_api_key = f.read()
 
 api_uri = "https://www.googleapis.com/youtube/v3/search?part=snippet&q={}&type=video&key={}"
 
-def get_video_info(query : str, title_append = "") -> dict:
+
+def get_video_info(query: str, title_append="") -> dict:
     """ 
     Retrives video ID from the youtube API 
 
@@ -20,23 +22,27 @@ def get_video_info(query : str, title_append = "") -> dict:
 
     query = query.replace(" ", "+")
     result = requests.get(api_uri.format(query, yt_api_key)).json()
-    
-    video_id = result["items"][0]["id"]["videoId"]
-    raw_title = result["items"][0]["snippet"]["title"]
 
-    return {"id" : video_id, "title" : raw_title + title_append}
+    return result
+
+    # video_id = result["items"][0]["id"]["videoId"]
+    # raw_title = result["items"][0]["snippet"]["title"]
+
+    # return {"id" : video_id, "title" : raw_title + title_append}
 
 
-def get_watch_url(video_id : str) -> str:
+def get_watch_url(video_id: str) -> str:
     """ Creates watchable / downloadable URL from video's ID """
 
     return "https://www.youtube.com/watch?v={}".format(video_id)
 
-def download_video(video_url, video_title, directory = os.getcwd()):
-    """ Uses pafy to download the video """ 
+
+def download_video(video_url, video_title, directory=os.getcwd()):
+    """ Uses pafy to download the video """
 
     video = pafy.new(video_url)
-    hq_video = video.getbest(preftype = "mp4")
+    hq_video = video.getbest(preftype="mp4")
 
-    # This bit is ugly but it works so ¯\_(ツ)_/¯ 
-    hq_video.download(filepath = directory + video_title + "." + hq_video.extension, quiet=False)
+    # This bit is ugly but it works so ¯\_(ツ)_/¯
+    hq_video.download(filepath=directory + video_title +
+                      "." + hq_video.extension, quiet=False)
